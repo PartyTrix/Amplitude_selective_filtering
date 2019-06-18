be240802bb5c9751b524c7002c7e32eb8d1f426d
PartyTrix <47160149+PartyTrix@users.noreply.github.com>
2019-06-18 12:13:18 +0200

    Update ASF.py

--- ASF.py
+++ ASF.py
@@ -1,22 +1,30 @@
 import numpy as np
 
-def amplitudeSelectiveFiltering(rgbvalues, amax = 0.002, delta = 0.0001):
-    rgbvalues = np.transpose(rgbvalues)
-    L = rgbvalues.shape[1]
-    
-    C = (1/(np.mean(rgbvalues,1)))
+def amplitudeSelectiveFiltering(C_rgb, amax = 0.002, delta = 0.0001):
+    '''
+    Input: Raw RGB signals with dimensions 3xL, where the R channel is column 0
+    Output: Filtered RGB-signals
+    '''
+    rgbvalues = np.transpose(C_rgb)
+    L = C_rgb.shape[1]
+    C = (1/(np.mean(C_rgb,1)))
 
-    Clong = np.transpose(np.array([C,]*(L)))
-    C = rgbvalues * Clong -1
-        
+    C = np.transpose(np.array([C,]*(L)))
+    #line 1
+    C = rgbvalues * C -1 
+    #line 2       
     F = np.abs(np.fft.fft(C,n=L,axis=1)/L) #kleine error met Matlab.. 
-       
-    W = (delta / np.abs(F[0,:]))
+    #line 3   
+    W = (delta / np.abs(F[0,:])) #F[0,:]  is the R-channel
+    #line 4
     W[np.abs(F[0,:]<amax)] = 1
+    
     W = W.reshape([1,L])
     test = np.tile(W,[3,1])
+    #line 5
     Ff = np.multiply(F,(test))
-        
+    
+    #line 6
     C = np.transpose(np.array([(np.mean(rgbvalues,1)),]*(L))) * np.abs(np.fft.ifft(Ff)+1)
  
 return np.transpose(C)
